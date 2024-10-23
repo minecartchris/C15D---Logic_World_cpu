2b0b4dcd4e2387567340f0a4973320a0f0faa147
minecartchris <130516132+minecartchris@users.noreply.github.com>
2024-10-23 17:12:45 -0500

    Update C15D.asm

--- C15D.asm
+++ C15D.asm
@@ -1,19 +1,19 @@
 #ruledef
 {
-    NOP =>0000
-    LDA => 0001
-    LDB => 0010
-    Or => 0011
-    RDA =>0100
-    RDB => 0101
-    JMP => 0110
-    TCB => 0111
-    SUB => 1000
-    ALUO => 1001
-    And => 1010
-    io => 1011
-    Xor => 1100
-    ADD => 1101
-    TCA => 1110
-    RST => 1111
+    nop =>  0b0000
+    lda => 0b0001
+    ldb => 0b0010
+    or => 0b0011
+    rda => 0b0100
+    rdb => 0b0101
+    jmp => 0b0110
+    tcb => 0b0111
+    sub => 0b1000
+    aluo => 0b1001
+    and => 0b1010
+    io => 0b1011
+    xor => 0b1100
+    add => 0b1101
+    tca => 0b1110
+    rst => 0b1111
 }
